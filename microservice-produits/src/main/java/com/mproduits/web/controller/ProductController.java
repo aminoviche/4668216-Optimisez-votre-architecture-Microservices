fb96805d0de4c39af6bee3aab506288ad0955975
aminoviche <aminoviche@gmail.com>
2023-04-29 16:06:30 +0100

    add poperties conf for mproduits

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.mproduits.web.controller;
 
+import com.mproduits.configuration.ApplicationPropertiesConfiguration;
 import com.mproduits.dao.ProductDao;
 import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
@@ -17,6 +18,14 @@ public class ProductController {
     @Autowired
     ProductDao productDao;
 
+    private final ApplicationPropertiesConfiguration appProperties;
+
+    public ProductController(ProductDao productDao, ApplicationPropertiesConfiguration appProperties){
+        this.productDao = productDao;
+        this.appProperties = appProperties;
+    }
+
+
     // Affiche la liste de tous les produits disponibles
     @GetMapping(value = "/Produits")
     public List<Product> listeDesProduits(){
@@ -25,7 +34,9 @@ public class ProductController {
 
         if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
 
-        return products;
+        List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
+
+        return listeLimitee;
 
     }
 
